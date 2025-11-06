e64cbc8994630362c5805809fdddf7c3a5bca7f4
Noluthando Mphande <noluthandowendy2013@gmail.com>
2025-11-06 12:27:40 +0200

    toggle button removed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Resume loaded successfully!");
+ // Dark Mode Toggle
+    function toggleMode() {
+      document.body.classList.toggle('dark');
+    }
+
+    // Scroll Animation
+    const sections = document.querySelectorAll('.section');
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) entry.target.classList.add("show");
+        });
+      },
+      { threshold: 0.2 }
+    );
+
+    sections.forEach((section) => observer.observe(section));
