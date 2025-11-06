0ea582f6a8a855e64352797d8d4e675cb4fc4dfd
Noluthando Mphande <noluthandowendy2013@gmail.com>
2025-11-06 15:38:47 +0200

    dark mode added

--- index.js
+++ index.js
@@ -11,3 +11,16 @@
     );
 
     sections.forEach((section) => observer.observe(section));
+
+    // Dark/Light Mode Toggle
+    const toggleBtn = document.getElementById("themeToggle");
+
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+      toggleBtn.textContent = "☀️ Light Mode";
+    } else {
+      toggleBtn.textContent = "🌙 Dark Mode";
+    }
+  });
